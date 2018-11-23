3996f5593cf84f53049997f3f60de1576e432cba
jschouw <45239548+jschouw@users.noreply.github.com>
2018-11-22 20:36:26 -0500

    Close #1

---
+++ rptl.py
@@ -0,0 +1,8 @@
+import click
+
+
+@click.command()
+def cli():
+    """Take time lapses with consistent settings across all frames to prevent flicker. Uses PiCamera on Raspberry Pi."""
+    pass
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='rptl',
+    version='0.1',
+    py_modules=['rptl'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        rptl=rptl:cli
+    ''',
+)
